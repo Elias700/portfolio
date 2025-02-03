2bc562f943015b02b1bc94b8ddbfd16e20abfa55
Elias700 <elias_ribeiro07@outlook.com.br>
2025-02-03 17:28:14 -0300

    ajustes menu & home

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -10,23 +10,27 @@ const Menu = () => {
     };
 
     return (
-        <header className='container'>
-            <nav className='navigation'>
-                <div className='hamburguer' onClick={toggleMenu}>
-                    <span className={menuOpen ? "line open" : "line"}></span>
-                    <span className={menuOpen ? "line open" : "line"}></span>
-                    <span className={menuOpen ? "line open" : "line"}></span>
-                </div>
-                <ul className={menuOpen ? "menu open" : "menu"}>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">Sobre</a></li>
-                    <li><a href="#">Habilidades</a></li>
-                    <li><a href="#">Projetos</a></li>
-                    <li><a href="#">Contato</a></li>
-                </ul>
-                <p className='logo'>Elias Riberio</p>
-            </nav>  
-        </header>
+        <div >
+           <header className='container'>
+                <nav className='navigation'>
+                    <div className='hamburguer' onClick={toggleMenu}>
+                        <span className={menuOpen ? "line open" : "line"}></span>
+                        <span className={menuOpen ? "line open" : "line"}></span>
+                        <span className={menuOpen ? "line open" : "line"}></span>
+                    </div>
+                    <ul className={menuOpen ? "menu open" : "menu"}>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Sobre</a></li>
+                        <li><a href="#">Habilidades</a></li>
+                        <li><a href="#">Projetos</a></li>
+                        <li><a href="#">Contato</a></li>
+                    </ul>
+                    <p className='logo'>Elias Riberio</p>
+                </nav> 
+           </header>
+
+        
+        </div> 
     );
 };
 
